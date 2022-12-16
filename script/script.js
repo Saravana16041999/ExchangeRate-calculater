810d3753273ff2ea912c1299a6ef0461814074f7
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-12-16 22:02:38 +0530

    completed

--- script/script.js
+++ script/script.js
@@ -10,38 +10,47 @@ const CurrencyValue = document.getElementById(`Allrate`)
 
 
 
-function Optionsall(){
-    fetch(`https://v6.exchangerate-api.com/v6/b552bb56953fe0ec2bc546e1/latest/USD`).then((res)=> res.json())
+function CompleteCalculation(){
+    
+    let Link = `https://v6.exchangerate-api.com/v6/b552bb56953fe0ec2bc546e1/latest/USD`
+    fetch(Link).then((res)=> res.json())
     .then((data)=>{
-        rate = data.conversion_rates
-        currency = Object.keys(rate)
+        rate = data.conversion_rates;
+        currency = Object.keys(rate);
         currency.forEach(element => {
             newOption1 = document.createElement('option');
-            CurrenyOne.appendChild(newOption1)
+            CurrenyOne.appendChild(newOption1);
             newOption1.innerText = element;
+            newOption1.value = element;
             newOption2 = document.createElement('option');
-            CurrenyTwo.appendChild(newOption2)
-            newOption2.innerText = element
-        });
+            CurrenyTwo.appendChild(newOption2);
+            newOption2.innerText = element;
+            newOption2.value = element;
+            
+        })
+        let currencyone = CurrenyOne.value;
+        let currencytwo = CurrenyTwo.value;
+        fetch(`https://v6.exchangerate-api.com/v6/b552bb56953fe0ec2bc546e1/latest/${currencyone}`)
+        .then((responce)=> responce.json()).then((obj)=>{
+            let rate2 = obj.conversion_rates[currencytwo];
+            CurrencyValue.innerText = `1 ${currencyone} = ${rate2.toFixed(2)}${currencytwo}`
+            AmountTwo.value = (Number(AmountOne.value)*rate2).toFixed(2)
+        })
     })
 }
 
-Optionsall()
+CompleteCalculation()
+
+CurrenyOne.addEventListener('change',CompleteCalculation)
+CurrenyTwo.addEventListener('change',CompleteCalculation)
+AmountOne.addEventListener('input',CompleteCalculation)
+AmountTwo.addEventListener('input',CompleteCalculation)
 
 
-function ExchangeRate(){
-    currencyone = CurrenyOne.value
-    fetch(`https://v6.exchangerate-api.com/v6/b552bb56953fe0ec2bc546e1/latest/USD`)
-    .then((resp)=> resp.json())
-    .then((data)=>{
-        console.log(data)
-    })
-}
-
-ExchangeRate()
 
 
 swap.addEventListener('click',()=>{
     [CurrenyOne.value,CurrenyTwo.value] = [CurrenyTwo.value,CurrenyOne.value];
+    CompleteCalculation()
 
 })
